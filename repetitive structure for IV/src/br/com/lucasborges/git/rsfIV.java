0cd684e03ad4079aa7204de4e9764fa0bd8aed54
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-21 21:52:09 -0300

    Add files via upload

---
+++ repetitive structure for IV/src/br/com/lucasborges/git/rsfIV.java
@@ -0,0 +1,28 @@
+package br.com.lucasborges.git;
+import java.util.Scanner;
+
+public class rsfIV {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			if (y == 0) {
+				System.out.println("divisao impossivel");
+			}
+			else {
+				double div = (double) x / y;
+				System.out.printf("%.1f%n", div);
+			}
+		}
+		
+		sc.close();
+	}
+}
